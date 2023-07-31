5bdea9164b99436f28f2ebf1e9c1edd1512c1ad1
TheOneProgrammerGuy <johnkennethgamora77@gmail.com>
2023-07-31 17:55:49 +0800

    added logo and fixed responsiveness

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
   return (
     <div className='text-white'>
         <div className='m-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center'>
-            <p className='md:text-4xl sm:text-3xl text-1xl text-[#00df9a] font-bold p-2 uppercase'>Hello and Welcome</p>
+            <p className='md:text-4xl sm:text-3xl text-1xl text-[#00DED3] font-bold p-2 '>Hello and Welcome</p>
             <h1 className='md:text-7xl sm:text-6xl text-4xl font-bold md:py-6'>I am John Kenneth Gamora</h1>
             <div className='flex justify-center items-center'>
                 <p className='md:text-5xl sm:text-4xl text-2xl font-bold'>An aspiring </p>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React, {useState} from "react";
 import {IoMdClose,IoMdMenu} from 'react-icons/io'
+import {TbSquareRoundedLetterG} from 'react-icons/tb'
 
 const Navbar = () => {
         const [isOpen, setIsOpen] = useState(false)
@@ -9,7 +10,10 @@ const Navbar = () => {
         }
     return (
         <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white'>
-            <h1 className='w-full text-3xl font-bold text-[#00df9a]'>React.</h1>
+            <div id="webLogo" className="flex justify-between items-center h-24 max-w-[1240px] px-4 text-white">
+                <TbSquareRoundedLetterG className='text-6xl font-bold text-[#00DED3]'/>
+                <h1 className='w-full text-3xl font-bold text-[#00DED3]'>amora</h1>
+            </div>
             <ul className='hidden md:flex'>
                 <li className='p-4'>Home</li>
                 <li className='p-4'>About</li>
@@ -19,8 +23,11 @@ const Navbar = () => {
             <div onClick={handleMenu} className='block md:hidden'>
                 {isOpen ? <IoMdClose className='text-3xl cursor-pointer'/> : <IoMdMenu className='text-3xl cursor-pointer'/>}
             </div>
-            <div className={isOpen ? 'fixed left-0 top-0 w-{60%} h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500' : 'fixed left-[-100%]'}>
-                <h1 className='w-full text-3xl font-bold text-[#00df9a] m-8'>React.</h1>
+            <div className={isOpen ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500' : 'fixed left-[-100%]'}>
+                <div id="webLogo" className="flex justify-between items-center h-24 max-w-[1240px] px-4 text-white">
+                    <TbSquareRoundedLetterG className='text-6xl font-bold text-[#00DED3]'/>
+                    <h1 className='w-full text-3xl font-bold text-[#00DED3]'>amora</h1>
+                </div>
                     <ul className='p-4 uppercase'>
                         <li className='p-4 border-b border-gray-600'>Home</li>
                         <li className='p-4 border-b border-gray-600'>About</li>
