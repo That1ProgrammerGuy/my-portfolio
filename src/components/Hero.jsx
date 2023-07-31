5e261b49dd29e9c5ca921266c3d2c37584c189b1
TheOneProgrammerGuy <johnkennethgamora77@gmail.com>
2023-07-31 16:51:46 +0800

    fixed bugs

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,11 +5,11 @@ const Hero = () => {
   return (
     <div className='text-white'>
         <div className='m-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center'>
-            <p className='text-[#00df9a] font-bold p-2 uppercase'>Hello and Welcome</p>
+            <p className='md:text-4xl sm:text-3xl text-1xl text-[#00df9a] font-bold p-2 uppercase'>Hello and Welcome</p>
             <h1 className='md:text-7xl sm:text-6xl text-4xl font-bold md:py-6'>I am John Kenneth Gamora</h1>
             <div className='flex justify-center items-center'>
                 <p className='md:text-5xl sm:text-4xl text-2xl font-bold'>An aspiring </p>
-                <Typed className='md:text-5xl sm:text-4xl text-2xl font-bold pl-2' 
+                <Typed className='md:text-5xl sm:text-4xl text-2xl font-bold md:pl-4 pl-2' 
                 strings={['Front-end Developer','Back-end Developer' , 'Video Editor', 'Game Developer', 'Software Engineer']} 
                 typeSpeed={60} 
                 loop />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,9 +17,9 @@ const Navbar = () => {
                 <li className='p-4'>Contact</li>
             </ul>
             <div onClick={handleMenu} className='block md:hidden'>
-                {!isOpen ? <IoMdClose className='text-3xl cursor-pointer'/> : <IoMdMenu className='text-3xl cursor-pointer'/>}
+                {isOpen ? <IoMdClose className='text-3xl cursor-pointer'/> : <IoMdMenu className='text-3xl cursor-pointer'/>}
             </div>
-            <div className={!isOpen ? 'fixed left-0 top-0 w-{60%} h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500' : 'fixed left-[-100%]'}>
+            <div className={isOpen ? 'fixed left-0 top-0 w-{60%} h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500' : 'fixed left-[-100%]'}>
                 <h1 className='w-full text-3xl font-bold text-[#00df9a] m-8'>React.</h1>
                     <ul className='p-4 uppercase'>
                         <li className='p-4 border-b border-gray-600'>Home</li>
